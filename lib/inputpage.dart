e66af6992a38472e7dc97f788f0bccb182e69adf
Duncan Kariuki <dkariuki@innovexsolutions.co.ke>
2020-10-05 13:39:12 +0300

    initial

---
+++ lib/inputpage.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+const bottomContainerHeight = 80.0;
+const bottomContainerColor = Color(0xFFEB1555);
+const Color reusableCardColor = Color(0xFF1D1E33);
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                    child: ReusableCard(
+                  colour: reusableCardColor,
+                )),
+                Expanded(
+                    child: ReusableCard(
+                  colour: reusableCardColor,
+                )),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(
+              colour: reusableCardColor,
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    colour: reusableCardColor,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: reusableCardColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: bottomContainerColor,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.colour});
+  final Color colour;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
+  }
+}
